52551e5051f493f2815dc2ad53cad1f46d4aac69
planetarian <1724041+planetarian@users.noreply.github.com>
2026-07-31 23:45:57 -0500

    MessageQueue patch instead of http intercept

--- dist/twit-fixer/plugin.js
+++ dist/twit-fixer/plugin.js
@@ -24,39 +24,70 @@
     onUnload: () => onUnload
   });
   var {
-    http: {
-      intercept
+    before
+  } = shelter.plugin.scoped.patcher;
+  var TWITTER_REGEX = /(?<pre>https?:\/\/)(?:x|(?:(?:v|f)x)?twitter)\.com(?<post>\/\w+\/status\/\d+(?:\/photo(?:\/(?<photonum>\d+)?)?)?)(?:\/en)*(?<query>(?:\?$|[a-zA-Z0-9\.\,\;\?\'\\\+&%\$\=~_\-\*]+))?(?<fragment>#[a-zA-Z0-9\-\.]+)?/gi;
+  var unpatch;
+  function replaceTwitterLinks(content) {
+    if (!content)
+      return content;
+    const regex = TWITTER_REGEX;
+    if (!regex.test(content))
+      return content;
+    content = content.replace(regex, (match, pre, post) => pre + "fxtwitter.com" + post + "/en");
+    if (content.length > 2e3) {
+      console.log("Can't replace twitter link; message would be too long.");
+      return null;
     }
-  } = shelter;
-  var unintercept;
-  async function handleDispatch(req, send) {
-    console.log("req:");
-    console.log(req);
-    try {
-      let content = req.body.content;
-      let regex = /(?<pre>https?:\/\/)(?:x|(?:(?:v|f)x)?twitter)\.com(?<post>\/\w+\/status\/\d+(?:\/photo(?:\/(?<photonum>\d+)?)?)?)(?:\/en)*(?<query>(?:\?$|[a-zA-Z0-9\.\,\;\?\'\\\+&%\$\=~_\-\*]+))?(?<fragment>#[a-zA-Z0-9\-\.]+)?/gi;
-      if (content && regex.test(content)) {
-        const replace = (match, pre, post, photonum, query, fragment, rest) => pre + "fxtwitter.com" + post + "/en";
-        content = content.replace(regex, replace);
-        if (content.length > 2e3) {
-          console.log("Can't replace twitter link; message would be too long.");
-          return;
+    console.log("Replaced twitter link: " + content);
+    return content;
+  }
+  function findPatchTarget(m) {
+    if (typeof m === "function" && m.prototype?.enqueue && m.prototype?.handleEdit)
+      return m.prototype;
+    if (m?.enqueue && m?.handleEdit && m?.logger?.name === "MessageQueue")
+      return m;
+    return null;
+  }
+  function findMessageQueue() {
+    let found;
+    webpackChunkdiscord_app.push([[Symbol()], {}, (req) => {
+      for (const id in req.c) {
+        const exp = req.c[id]?.exports;
+        if (!exp)
+          continue;
+        for (const key of Object.keys(exp)) {
+          const t2 = findPatchTarget(exp[key]);
+          if (t2)
+            found = t2;
         }
-        req.body.content = content;
-        console.log("Replaced twitter link: " + content);
+        const t = findPatchTarget(exp.default);
+        if (t)
+          found = t;
       }
-    } catch (err) {
-      console.log(err);
-    }
-    return await send(req);
+    }]);
+    return found;
   }
   function onLoad() {
-    unintercept = intercept("post", /\/channels\/\d+\/messages/, handleDispatch);
-    console.log("twit-fixer loaded");
+    const target = findMessageQueue();
+    if (!target)
+      return console.log("[twit-fixer] MessageQueue not found");
+    unpatch = before("enqueue", target, (args) => {
+      if (args[0]?.type !== 0)
+        return;
+      try {
+        const fixed = replaceTwitterLinks(args[0].message?.content);
+        if (fixed != null)
+          args[0].message.content = fixed;
+      } catch (err) {
+        console.log("[twit-fixer] Error replacing twitter links:", err);
+      }
+    });
+    console.log("[twit-fixer] loaded");
   }
   function onUnload() {
-    unintercept();
-    console.log("twit-fixer unloaded");
+    unpatch?.();
+    console.log("[twit-fixer] unloaded");
   }
   return __toCommonJS(twit_fixer_exports);
 })();
--- plugins/twit-fixer/index.jsx
+++ plugins/twit-fixer/index.jsx
@@ -1,43 +1,68 @@
-const {
-	http: {
-		intercept
+const { before } = shelter.plugin.scoped.patcher;
+
+const TWITTER_REGEX = /(?<pre>https?:\/\/)(?:x|(?:(?:v|f)x)?twitter)\.com(?<post>\/\w+\/status\/\d+(?:\/photo(?:\/(?<photonum>\d+)?)?)?)(?:\/en)*(?<query>(?:\?$|[a-zA-Z0-9\.\,\;\?\'\\\+&%\$\=~_\-\*]+))?(?<fragment>#[a-zA-Z0-9\-\.]+)?/gi;
+
+let unpatch;
+
+function replaceTwitterLinks(content) {
+	if (!content) return content;
+	const regex = TWITTER_REGEX;
+	if (!regex.test(content)) return content;
+	content = content.replace(regex, (match, pre, post) => pre + "fxtwitter.com" + post + "/en");
+	if (content.length > 2000) {
+		console.log("Can't replace twitter link; message would be too long.");
+		return null;
 	}
-} = shelter;
+	console.log("Replaced twitter link: " + content);
+	return content;
+}
 
-var unintercept;
+// shelter's http interceptor is not working, so we need a different way of intercepting messages.
+// This method patches MessageQueue's enqueue which is about as narrow a scope as we can hope for at the moment.
 
-async function handleDispatch(req, send) {
-    // only listen for message_create in the current channel
-	console.log("req:");
-	console.log(req);
+function findPatchTarget(m) {
+	if (typeof m === "function" && m.prototype?.enqueue && m.prototype?.handleEdit)
+		return m.prototype;
+	if (m?.enqueue && m?.handleEdit && m?.logger?.name === "MessageQueue")
+		return m;
+	return null;
+}
 
-	try {
-		let content = req.body.content;
-		let regex = /(?<pre>https?:\/\/)(?:x|(?:(?:v|f)x)?twitter)\.com(?<post>\/\w+\/status\/\d+(?:\/photo(?:\/(?<photonum>\d+)?)?)?)(?:\/en)*(?<query>(?:\?$|[a-zA-Z0-9\.\,\;\?\'\\\+&%\$\=~_\-\*]+))?(?<fragment>#[a-zA-Z0-9\-\.]+)?/gi;
-		if (content && regex.test(content)) {
-			const replace = (match, pre, post, photonum, query, fragment, rest) => pre + "fxtwitter.com" + post + "/en";
-			content = content.replace(regex, replace);
-			if (content.length > 2000) {
-				console.log("Can't replace twitter link; message would be too long.");
-				return; 
+function findMessageQueue() {
+	let found;
+	webpackChunkdiscord_app.push([[Symbol()], {}, (req) => {
+		for (const id in req.c) {
+			const exp = req.c[id]?.exports;
+			if (!exp) continue;
+			for (const key of Object.keys(exp)) {
+				const t = findPatchTarget(exp[key]);
+				if (t) found = t;
 			}
-			req.body.content = content;
-			console.log("Replaced twitter link: " + content);
+			const t = findPatchTarget(exp.default);
+			if (t) found = t;
 		}
-	}
-	catch (err) {
-		console.log(err);
-	}
-	
-	return await send(req);
+	}]);
+	return found;
 }
 
-export function onLoad() { // optional
-    unintercept = intercept("post", /\/channels\/\d+\/messages/, handleDispatch);
-	console.log("twit-fixer loaded")
+export function onLoad() {
+	const target = findMessageQueue();
+	if (!target) return console.log("[twit-fixer] MessageQueue not found");
+
+	unpatch = before("enqueue", target, (args) => {
+		if (args[0]?.type !== 0) return;
+		try {
+			const fixed = replaceTwitterLinks(args[0].message?.content);
+			if (fixed != null) args[0].message.content = fixed;
+		}
+		catch (err) {
+			console.log("[twit-fixer] Error replacing twitter links:", err);
+		}
+	});
+	console.log("[twit-fixer] loaded");
 }
 
-export function onUnload() { // required
-    unintercept();
-	console.log("twit-fixer unloaded")
+export function onUnload() {
+	unpatch?.();
+	console.log("[twit-fixer] unloaded");
 }
